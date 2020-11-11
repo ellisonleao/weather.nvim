41154124ba665b0ec914624abd3b713b942d4d4b
Ellison Leão <ellisonleao@gmail.com>
2020-11-11 11:34:35 -0300

    little refactoring

--- lua/weather.lua
+++ lua/weather.lua
@@ -1,7 +1,5 @@
--- Weather module for weather.nvim plugie
-local api = vim.api
+-- Weather module for weather.nvim plugin
 local win, buf
-
 local M = {}
 
 -- creates :Weather command
@@ -10,15 +8,10 @@ local function create_command()
     "command! -bang -nargs=? Weather lua require('weather').show_weather(<f-args>)")
 end
 
--- creates mappings for floating window
-local function create_mappings()
-end
-
--- creates a buffer window with weather output
-function M.show_weather(city)
+local function create_window()
   -- window size and pos
-  local width = api.nvim_get_option("columns")
-  local height = api.nvim_get_option("lines")
+  local width = vim.api.nvim_get_option("columns")
+  local height = vim.api.nvim_get_option("lines")
   local win_height = math.ceil(height * 0.6 - 8)
   local win_width = math.ceil(width * 0.3 - 6)
   local x_pos = 1
@@ -34,18 +27,25 @@ function M.show_weather(city)
   }
 
   -- create preview buffer and set local options
-  buf = api.nvim_create_buf(false, true)
-  win = api.nvim_open_win(buf, true, win_opts)
+  buf = vim.api.nvim_create_buf(false, true)
+  win = vim.api.nvim_open_win(buf, true, win_opts)
 
   -- create mapping to close buffer
-  api.nvim_buf_set_keymap(buf, "n", "q", ":lua require('weather').close_window()<cr>",
-                          {noremap = true, silent = true})
+  vim.api.nvim_buf_set_keymap(buf, "n", "q",
+                              ":lua require('weather').close_window()<cr>",
+                              {noremap = true, silent = true})
 
   -- kill buffer on close
-  api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
-  api.nvim_win_set_option(win, "winblend", 80)
+  vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+  vim.api.nvim_win_set_option(win, "winblend", 80)
+end
+
+-- creates a buffer window with weather output
+function M.show_weather(city)
+  create_window()
 
   -- handle city global param
+  -- can either be g:weather_city or a param in :Weather command
   local city_param
   if vim.g.weather_city ~= nil then
     city_param = vim.g.weather_city
@@ -56,18 +56,17 @@ function M.show_weather(city)
   end
 
   local command = string.format("curl https://wttr.in/%s?0", city_param)
-  api.nvim_call_function("termopen", {command})
+  vim.api.nvim_call_function("termopen", {command})
 end
 
 -- closes floating window
 function M.close_window()
-  api.nvim_win_close(win, true)
+  vim.api.nvim_win_close(win, true)
 end
 
 -- main function
 function M.init()
   create_command()
-  create_mappings()
 end
 
 return M
